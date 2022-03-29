9236ced813f766eda81660f9d5809d159354d276
Francesco Lorenzetti <francesco.lorenzetti@pm.me>
2022-03-29 14:25:27 +0100

    packaging

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function prettyBreak(element, separator = "<br>") {
+export function prettyBreak(element, separator = '<br>') {
   const text = element.textContent;
   const elementWidth = element.scrollWidth;
   // measure text width in one line
-  const textWrapper = document.createElement("span");
-  textWrapper.style.whiteSpace = "nowrap";
+  const textWrapper = document.createElement('span');
+  textWrapper.style.whiteSpace = 'nowrap';
   textWrapper.innerText = text;
   element.innerHTML = textWrapper.outerHTML;
   // check if text needs breaking
@@ -16,8 +16,8 @@ function prettyBreak(element, separator = "<br>") {
   // break in the middle
   if (textWidth > elementWidth) {
     let middle = Math.floor(text.length / 2);
-    const before = text.lastIndexOf(" ", middle);
-    const after = text.indexOf(" ", middle + 1);
+    const before = text.lastIndexOf(' ', middle);
+    const after = text.indexOf(' ', middle + 1);
     if (middle - before < after - middle) {
       middle = before;
     } else {
@@ -25,7 +25,7 @@ function prettyBreak(element, separator = "<br>") {
     }
     const s1 = text.substr(0, middle);
     const s2 = text.substr(middle + 1);
-    element.innerHTML = s1 + separator + " " + s2;
+    element.innerHTML = s1 + separator + ' ' + s2;
     return;
   }
   // no need for breaking
